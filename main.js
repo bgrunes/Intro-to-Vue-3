e5802dc3507a6a6f44c611a53d26792163c7c234
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2025-03-06 11:06:06 -0500

    L10 work

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeFromCart(id) {
+            this.cart = this.cart.filter(item => item.id === id)
+            console.log(this.cart)
+        }
+    }
 })
